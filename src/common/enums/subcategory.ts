59126c3d4fd09da116ff67156ff4871826bdb4f5
Chuck JS. Oh <jinseokoh@hotmail.com>
2023-05-29 11:37:20 +0900

    add DRIVE to hobby category

--- src/common/enums/subcategory.ts
+++ src/common/enums/subcategory.ts
@@ -1,31 +1,15 @@
 export enum SubCategory {
-  // LEISURE
-  DINING = 'dining', // 식사
-  CAFE = 'cafe', // 커피,카페
-  ALCOHOL = 'alcohol', // 술
-  MOVIE = 'movie', // 영화
-  THEATER = 'theater', // 연극
-  CONCERT = 'concert', // 콘서트
-  STADIUM = 'stadium', // 스포츠관람
-  EXHIBITION = 'exhibition', // 전시공연
-  PARK = 'park', // 공원
-  OTHER_LEISURE = 'otherLeisure',
-  // HOBBY 기술 장비 준비물 지식 연습
-  DRAWING = 'drawing', // 일러스트
-  CRAFTING = 'crafting', // 공예,도자기
-  MUSICAL_INSTRUMENT = 'musicalInstrument', // 악기
-  KARAOKE = 'karaoke', // 노래방
-  DANCING = 'dancing', // KPOP댄스
-  GAME = 'game', // 게임
-  FISHING = 'fishing', // 낚시
-  CAMPING = 'camping', // 캠핑
-  TRAVEL = 'travel', // 여행
-  COOKING = 'cooking', // 요리
-  WRITING = 'writing', // 글쓰기,출판
-  PHOTO_SHOOTING = 'photoShooting', // 사진출사
-  PLANTING = 'planting', // 반려식물
-  PET = 'pet', // 반려동물
-  OTHER_HOBBY = 'otherHobby',
+  // CHALLENGE 도전
+  STUDY = 'study', // 공부
+  BOOK = 'book', // 독서
+  LANGUAGE = 'language', // 외국어,언어교환
+  CODING = 'coding', // 코딩
+  YOUTUBER = 'youtuber', // 유튜브창업
+  ENTREPRENEURSHIP = 'entrepreneurship', // 창업
+  STOCKS = 'stocks', // 주식,재테크
+  ONE_DAY_CLASS = 'oneDayClass', // 일일체험
+  VOLUNTARY_ACTIVITY = 'voluntaryActivity', // 봉사
+  OTHER_CHALLENGE = 'otherChallenge', // 기타
   // SPORTS
   BASKETBALL = 'basketball', // 농구
   VOLLEYBALL = 'volleyball', // 배구
@@ -47,15 +31,32 @@ export enum SubCategory {
   RUNNING = 'running', // 달리기
   GYM = 'gym', // 헬스,요가,필라테스
   OTHER_SPORTS = 'otherSports',
-  // OTHER 도전
-  STUDY = 'study', // 공부
-  BOOK = 'book', // 독서
-  LANGUAGE = 'language', // 외국어,언어교환
-  CODING = 'coding', // 코딩
-  YOUTUBER = 'youtuber', // 유튜브창업
-  ENTREPRENEURSHIP = 'entrepreneurship', // 창업
-  STOCKS = 'stocks', // 주식,재테크
-  ONE_DAY_CLASS = 'oneDayClass', // 일일체험
-  VOLUNTARY_ACTIVITY = 'voluntaryActivity', // 봉사
-  OTHER_CHALLENGE = 'otherChallenge', // 기타
+  // LEISURE
+  DINING = 'dining', // 식사
+  CAFE = 'cafe', // 커피,카페
+  ALCOHOL = 'alcohol', // 술
+  MOVIE = 'movie', // 영화
+  THEATER = 'theater', // 연극
+  CONCERT = 'concert', // 콘서트
+  STADIUM = 'stadium', // 스포츠관람
+  EXHIBITION = 'exhibition', // 전시공연
+  PARK = 'park', // 공원
+  OTHER_LEISURE = 'otherLeisure',
+  // HOBBY 기술 장비 준비물 지식 연습
+  DRAWING = 'drawing', // 일러스트
+  CRAFTING = 'crafting', // 공예,도자기
+  MUSICAL_INSTRUMENT = 'musicalInstrument', // 악기
+  KARAOKE = 'karaoke', // 노래방
+  DANCING = 'dancing', // KPOP댄스
+  GAME = 'game', // 게임
+  FISHING = 'fishing', // 낚시
+  CAMPING = 'camping', // 캠핑
+  TRAVEL = 'travel', // 여행
+  DRIVE = 'drive', // 드라이브
+  COOKING = 'cooking', // 요리
+  WRITING = 'writing', // 글쓰기,출판
+  PHOTO_SHOOTING = 'photoShooting', // 사진출사
+  PLANTING = 'planting', // 반려식물
+  PET = 'pet', // 반려동물
+  OTHER_HOBBY = 'otherHobby',
 }
